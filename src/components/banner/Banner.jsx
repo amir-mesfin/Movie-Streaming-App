18299d57165b377b0a0009d855f46d6060f970e9
abushe136 <amir.mesfin136@gmail.com>
2025-05-10 20:34:44 -0300

    Add and style homepage banner section with responsive design

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -9,6 +9,7 @@ function Banner() {
     async function fetchData(){
       try{
         const response = await axios.get(requests.fetchNetflixOriginals);
+        console.log(response)
         setMovie(
           response.data.results[
             Math.floor(Math.random() * response.data.results.length - 1)
@@ -19,26 +20,37 @@ function Banner() {
       }
     }
     fetchData();
-  })
+  },[]);
+  const truncate = (string,n)=>{
+    return string?.length >0 ? string.substr(0,n-1) + "..." : string;
+  }
   return (
     <div className={style["banner"]}
                 style={{
                   backgroundSize: "cover",
-                  backgroundImage: `url('https://image.thdb.org/t/p/original/${movie?.backdrop_path}')`,
+                  backgroundImage: `url('https://image.tmdb.org/t/p/original${movie?.backdrop_path}')`,
                   backgroundPosition: "center center",
                   backgroundRepeat: 'no-repeat'
                 }}>
-      <div className={style['banner_contents']}>
-        <h1 className={style["banner_title"]}>
+      <div className={style['banner__contents']}>
+        <h1 className={style['banner_title']}>
           {movie?.title || movie?.name || movie?.original_name}
         </h1>
-        <div className={style["banner_buttons"]}></div>
-          <button className={style["banner_button play"]}>Play</button>
-          <button className={style["banner_button"]}>My List</button>
+        <div className={style['banner__buttons']}>
+        <button className={`${style.banner__button} ${style.banner__play}`}>
+            <p>play</p>
+          </button>
+          <button className={`${style.banner__button} ${style.banner__info} ${style.hoverButton}`}>
+            <p>my list</p>
+          </button>
         </div>
+        <h1 className={style['banner__description']}> {truncate(movie?.overview, 1500)} </h1>
+        </div>
+        <div className={style['banner_fadeBOtom']} />
       </div>
   
   )
+
 }
 
 export default Banner
