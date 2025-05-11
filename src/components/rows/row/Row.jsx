2cd8326dbc48489b26ba64f4b7d154db57a4eabb
abushe136 <amir.mesfin136@gmail.com>
2025-05-11 10:00:19 -0300

    to set up for movies

---
+++ src/components/rows/row/Row.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import style from './row.module.css'
+function Row() {
+  return (
+    <div>
+      abushe
+    </div>
+  )
+}
+
+export default Row
---
+++ src/components/rows/rowList/RowList.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import style from './rowList.module.css'
+import Row from '../row/Row'
+function RowList() {
+  return (
+    <div>
+      <Row />
+      <Row />
+      <Row />
+      <Row />
+      <Row />
+      <Row />
+      <Row />
+    </div>
+  )
+}
+
+export default RowList
