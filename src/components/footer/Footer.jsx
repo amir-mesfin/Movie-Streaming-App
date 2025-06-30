d8394e5aafb96dd1255531fbdce8e431293830fd
abushe136 <amir.mesfin136@gmail.com>
2025-06-30 08:37:35 -0300

    final chnhe to heder and fotter part

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,56 +1,54 @@
-import React from 'react'
-import style from './Footer.module.css'
+import React from 'react';
+import style from './Footer.module.css';
 import FacebookIcon from '@mui/icons-material/Facebook';
 import InstagramIcon from '@mui/icons-material/Instagram';
 import YouTubeIcon from '@mui/icons-material/YouTube';
+
 function Footer() {
   return (
-    <div className={style["footer"]}>
-    <div className={style["footer__container"]}>
-      <div className={style["footer__icons"]}>
-        <FacebookIcon />
-        <InstagramIcon />
-        <YouTubeIcon />
-      </div>
-      <div className={style["footer__nav"]}>
-        <div>
-          <ul>
+    <div className={style.footer}>
+      <div className={style.stripBackground}></div>
+
+      <div className={style.footer__container}>
+        <div className={style.footer__icons}>
+          <FacebookIcon className={style.icon} />
+          <InstagramIcon className={style.icon} />
+          <YouTubeIcon className={style.icon} />
+        </div>
+
+        <div className={style.footer__nav}>
+          <div><ul>
             <li>Audio Description</li>
             <li>Investor Relations</li>
             <li>Legal Notice</li>
-          </ul>
-        </div>
-        <div>
-          <ul>
+          </ul></div>
+          <div><ul>
             <li>Help Center</li>
             <li>Jobs</li>
-            <li>Cookie Preference</li>
-          </ul>
-        </div>
-        <div>
-          <ul>
+            <li>Cookie Preferences</li>
+          </ul></div>
+          <div><ul>
             <li>Gift Cards</li>
             <li>Terms of Use</li>
-            <li>Corporate Information</li>
-          </ul>
-        </div>
-        <div>
-          <ul>
+            <li>Corporate Info</li>
+          </ul></div>
+          <div><ul>
             <li>Media Center</li>
             <li>Privacy</li>
             <li>Contact Us</li>
-          </ul>
+          </ul></div>
+        </div>
+
+        <div className={style.footer__serviceCode}>
+          <p>Service Code</p>
+        </div>
+
+        <div className={style.footer__copyWrite}>
+          <span>&copy; 2024 Movie-Streaming-App. All rights reserved.</span>
         </div>
       </div>
-      <div className={style["footer__serviceCode"]}>
-        <p>Service Code</p>
-      </div>
-      <div className={style["footer__copyWrite"]}>
-        <span>&copy; 1997 - 2024 Netflix, Inc.</span>
-      </div>
     </div>
-  </div>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,38 +1,57 @@
-import React from 'react'
-import style from './header.module.css'
-// import NetflixLogo from '../../assets/images/NetflixLogo.png'
-import NetflixLogo from '../../assets/images/NetflixLogo.png'
+import React, { useState } from 'react';
+import style from './header.module.css';
 import SearchIcon from '@mui/icons-material/Search';
 import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
 import AccountBoxIcon from '@mui/icons-material/AccountBox';
 import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import MenuIcon from '@mui/icons-material/Menu';
+import CloseIcon from '@mui/icons-material/Close';
+
 function Header() {
+  const [isMobileOpen, setIsMobileOpen] = useState(false);
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+  const toggleMobile = () => setIsMobileOpen(!isMobileOpen);
+  const toggleDropdown = () => setIsDropdownOpen(!isDropdownOpen);
+
   return (
     <div className={style["header-outer-container"]}>
-          <div className={style["header-container"]}>
-                  <div className={style["header-left"]}>
-                      <ul>
-                          <li> <img src={NetflixLogo} alt="movies-logo" width="100" /> </li>
-                          <li>MLA</li>
-                          <li>Home</li>
-                          <li>TvShow</li>
-                          <li>Movies</li>
-                          <li>Latest</li>
-                          <li>MYlist</li>
-                          <li>Browse By languages</li>
-                      </ul>
-                  </div>
-                  <div className={style["header-right"]}>
-                        <ul>
-                              <li> <SearchIcon/> </li>
-                              <li> <NotificationsNoneIcon/> </li>
-                              <li> <AccountBoxIcon /> </li>
-                              <li> <ArrowDropDownIcon/> </li>
-                          </ul>
-                  </div>
-          </div>
-     </div>
-  )
+      <div className={style["header-container"]}>
+        <div className={style["header-left"]}>
+          <ul className={`${style["nav-list"]} ${isMobileOpen ? style["mobile-show"] : ""}`}>
+            <li className={style["logo"]}>🎬 <span>Movie-Streaming-App</span></li>
+            <li>Home</li>
+            <li>TV Shows</li>
+            <li>Movies</li>
+            <li>Latest</li>
+            <li>My List</li>
+            <li>Languages</li>
+          </ul>
+        </div>
+
+        <div className={style["header-right"]}>
+          <ul>
+            <li><SearchIcon /></li>
+            <li><NotificationsNoneIcon /></li>
+            <li onClick={toggleDropdown} className={style["profile"]}>
+              <AccountBoxIcon />
+              <ArrowDropDownIcon />
+              {isDropdownOpen && (
+                <div className={style["dropdown"]}>
+                  <a href="#">Profile</a>
+                  <a href="#">Settings</a>
+                  <a href="#">Logout</a>
+                </div>
+              )}
+            </li>
+            <li className={style["hamburger"]} onClick={toggleMobile}>
+              {isMobileOpen ? <CloseIcon /> : <MenuIcon />}
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Header
+export default Header;
