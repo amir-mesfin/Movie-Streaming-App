8551a32a9fce94d0f49e1d046360c9de72328ec7
abushe136 <amir.mesfin136@gmail.com>
2025-07-28 10:08:06 +0300

    Fix vite base path for Netlify deploy

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Movie-Streaming-App/', // 👈 Add this line
+  base: '/', // 👈 Add this line
   plugins: [react()],
 })
